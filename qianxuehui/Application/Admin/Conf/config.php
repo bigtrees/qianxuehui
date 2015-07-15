a72c913dd78b72e38ead178f731b261c7f1cb2a6
iambigtree <1036872483@qq.com>
2015-07-16 00:18:04 +0800

    更新

--- qianxuehui/Application/Admin/Conf/config.php
+++ qianxuehui/Application/Admin/Conf/config.php
@@ -3,10 +3,10 @@ return array(
 	//'配置项'=>'配置值'
 	//// 数据库设置 
 	'DB_TYPE' => 'mysql', // 数据库类型
-	'DB_HOST' => 'localhost', // 服务器地址
-	'DB_USER' => 'root', // 数据库服务器用户名
-	'DB_PWD' => '', // 数据库服务器用户名密码
-	'DB_NAME' => 'qxhdb', // 数据库服务器数据库名
+	'DB_HOST' => '120.24.181.58', // 服务器地址
+	'DB_USER' => 'admin', // 数据库服务器用户名
+	'DB_PWD' => 'qianxuelian', // 数据库服务器用户名密码
+	'DB_NAME' => 'qianxuelian', // 数据库服务器数据库名
 	'DB_PORT' => '3306', // 端口
 	'DB_PREFIX' => '', // 数据库表前缀
 );
